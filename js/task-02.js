7662ff05db3435818e410279f132395dedf0964a
Denys Zabolotskyi <d.zabolotskyi@gmail.com>
2020-12-09 16:15:23 +0200

    task-02

---
+++ js/task-02.js
@@ -0,0 +1,30 @@
+const calculateEngravingPrice = function (message, pricePerWord) {
+  let priceTotal = message.split(' ').length * pricePerWord;
+  //   console.log(priceTotal);
+  return `Общая стоимость гравировки ${priceTotal}: BTC`;
+};
+
+/*
+ * Вызовы функции для проверки работоспособности твоей реализации.
+ */
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    10,
+  ),
+); // 80
+
+console.log(
+  calculateEngravingPrice(
+    'Proin sociis natoque et magnis parturient montes mus',
+    20,
+  ),
+); // 160
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 40),
+); // 240
+
+console.log(
+  calculateEngravingPrice('Donec orci lectus aliquam est magnis', 20),
+); // 120
